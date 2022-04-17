6d95257a4198361ae3fdc7bb68138ca06f846e03
Ivan Dario Posada <ivandarioposada@yahoo.com>
2022-04-16 23:33:10 -0500

    Home
    Add Feature Insights Section

--- index.php
+++ index.php
@@ -1,6 +1,70 @@
 <?php include 'header.php' ?>
 
 <main>
+  <h1> WE ARE BANKS VISUALIZED </h1>
+  <p class="home-subtitle"> Visualize banking data like never before.</p>
+
+
+    <div class="row">
+      <div class="col m4">
+          <div class="card">
+            <div class="card-image">
+              <img src="<?php bloginfo('template_url'); ?>/img/news1.png" alt="">
+            </div>
+            <div class="card-title">
+              Asset Information
+            </div>
+
+            <div class="card-action home-learn-more">
+              <a href="#">Learn More</a>
+
+            </div>
+
+
+          </div>
+      </div>
+
+      <div class="col m4">
+        <div class="card">
+          <div class="card-image">
+            <img src="<?php bloginfo('template_url'); ?>/img/news1.png" alt="">
+          </div>
+          <div class="card-title">
+            Asset Information
+          </div>
+
+          <div class="card-action home-learn-more">
+            <a href="#">Learn More</a>
+
+          </div>
+
+
+        </div>
+      </div>
+
+      <div class="col m4">
+        <div class="card">
+          <div class="card-image">
+            <img src="<?php bloginfo('template_url'); ?>/img/news1.png" alt="">
+          </div>
+          <div class="card-title">
+            Asset Information
+          </div>
+
+          <div class="card-action home-learn-more">
+            <a href="#">Learn More</a>
+
+          </div>
+
+
+        </div>
+      </div>
+
+    </div>
+
+
+
+
 
 </main>
 
