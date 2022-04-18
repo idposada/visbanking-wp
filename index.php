23a91181a0025882aade22bb0e037806a1bb0b07
Ivan Dario Posada <ivandarioposada@yahoo.com>
2022-04-17 21:26:04 -0500

    Add Home Sections
    Add and finish home sections and implemet required styles

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
     <div class="row">
       <div class="col m4">
-          <div class="card">
+          <div class="card vis-card">
             <div class="card-image">
               <img src="<?php bloginfo('template_url'); ?>/img/news1.png" alt="">
             </div>
@@ -25,7 +25,7 @@
       </div>
 
       <div class="col m4">
-        <div class="card">
+        <div class="card vis-card">
           <div class="card-image">
             <img src="<?php bloginfo('template_url'); ?>/img/news1.png" alt="">
           </div>
@@ -43,7 +43,7 @@
       </div>
 
       <div class="col m4">
-        <div class="card">
+        <div class="card vis-card">
           <div class="card-image">
             <img src="<?php bloginfo('template_url'); ?>/img/news1.png" alt="">
           </div>
@@ -61,6 +61,153 @@
       </div>
 
     </div>
+
+    <hr>
+    <div class="row">
+      <h2>Ways that Visbanking will change your perspective</h2>
+      <p class="home-subtitle"> SERVICES</p>
+    </div>
+
+    <div class="row">
+      <div class="col m4">
+        <div class="service-card">
+          <div class="service-card-title">
+            Competitive Analysis
+          </div>
+          <div class="service-card-text">
+            Our service allows you to quickly and easily view any group of competitor banks to assess and compare their performance. That intelligence can help you perform complex analysis easier than ever before, enabling better decision-making that will improve your company's bottom line. Quickly access details like balance sheets, financial statements, current activities, and insights in the news. With a few clicks, those and other key data points can be quickly compiled to inform your analysis.
+          </div>
+        </div>
+
+      </div>
+
+      <div class="col m4">
+        <div class="service-card">
+          <div class="service-card-title">
+            Market Share
+          </div>
+          <div class="service-card-text">
+          When you need to make critical decisions that impact your company's market share, it is vital to understand everything that you can about your competitors. With Visbanking's services, you can quickly define market parameters and get the intelligence you need to measure the market share of all banks in that target market and adapt your strategies to improve your bank's performance.
+          </div>
+        </div>
+
+      </div>
+
+      <div class="col m4">
+        <div class="service-card">
+          <div class="service-card-title">
+          M&A due dilligence
+          </div>
+          <div class="service-card-text">
+          Before any successful merger or acquisition, it is essential to perform due diligence on important criteria like credit quality metrics. If you have a merger looming on the horizon, our tools can help you gain the insight you need to know before moving to combine portfolios. This data can help ensure that your merger or acquisition can be completed without unforeseen complications.
+          </div>
+        </div>
+
+      </div>
+
+      <div class="col m4">
+        <div class="service-card">
+          <div class="service-card-title">
+            Credit Quality Trends
+          </div>
+          <div class="service-card-text">
+          It can be time-consuming to assess credit quality trends in the banking industry, including key information about things like non-performing loans viewed by asset class. Visbanking's services enable you to view any custom group of competitor banks in a way that simplifies your analysis of their comparative credit quality performance. In addition, we simplify the process of comparing past due loan trends and relevant reserves, enabling you to easily compare trends in 20 and 90-day past due loans alongside the allowance for loan and lease loss trends, to quickly identify anomalies.
+          </div>
+        </div>
+
+      </div>
+
+      <div class="col m4">
+        <div class="service-card">
+          <div class="service-card-title">
+            Geographic Coverage
+          </div>
+          <div class="service-card-text">
+          One of the biggest challenges to new market entry is learning about existing competition in that area. If you are considering a move into a new market, Visbanking can provide the information you need to accurately assess your banking competitors in that geographical area, including key details about those banks' deposit sources and lending participation.
+          </div>
+        </div>
+
+      </div>
+
+      <div class="col m4">
+        <div class="service-card">
+          <div class="service-card-title">
+            Insight into Deposit and Loan Growth or Decline
+          </div>
+          <div class="service-card-text">
+          Does your business need to learn more about deposit or loan growth or decline throughout the U.S.? Is a comprehensive understanding of macro trends a vital part of your decision-making process? If so, our services can provide you with the ability to quickly access information about the entire U.S. banking landscape, including deposit and loan growth over time, net interest margins, and the cost of funds, deposits, and non-interest income.
+          </div>
+        </div>
+
+      </div>
+
+
+    </div>
+
+    <hr>
+
+    <div class="row">
+      <h2>Check out the dashboards and visualizations</h2>
+    </div>
+
+    <div class="row">
+
+      <div class="col l6">
+        <img class="img-dashboards" src="<?php bloginfo('template_url'); ?>/img/gallery1.png" alt="">
+      </div>
+
+      <div class="col l6">
+        <img class="img-dashboards" src="<?php bloginfo('template_url'); ?>/img/gallery2.png" alt="">
+      </div>
+
+    </div>
+
+    <div class="row">
+
+      <div class="col l6">
+        <img class="img-dashboards" src="<?php bloginfo('template_url'); ?>/img/gallery3.png" alt="">
+      </div>
+
+      <div class="col l6">
+        <img class="img-dashboards" src="<?php bloginfo('template_url'); ?>/img/gallery4.png" alt="">
+      </div>
+    </div>
+    <div class="row">
+
+      <div class="col l6">
+        <img class="img-dashboards" src="<?php bloginfo('template_url'); ?>/img/gallery5.png" alt="">
+      </div>
+
+      <div class="col l6">
+        <img class="img-dashboards" src="<?php bloginfo('template_url'); ?>/img/gallery6.png" alt="">
+      </div>
+    </div>
+
+    <hr>
+
+    <div class="row newsletter-home">
+
+      <p class="news-title">Data delivered to your inbox </p>
+      <p class="news-text">Keep up with the latest data and most popular content. </p>
+
+      <form class="" action="index.html" method="post">
+
+        <div class="home-form-fields">
+          <input type="text" name="name" value="" placeholder="Your Name">
+          <input type="email" name="email" value="" placeholder="Your email">
+        </div>
+        <input type="submit" name="" value="SUSBCRIBE >>">
+      </form>
+
+    </div>
+
+    <hr>
+
+    <div class="row">
+      <h2>Featured News</h2>
+    </div>
+
+
 
 
 
