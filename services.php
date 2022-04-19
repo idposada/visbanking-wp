d6bb0ad731879533621ce629528f01a728c83c00
Ivan Dario Posada <ivandarioposada@yahoo.com>
2022-04-19 18:27:46 -0500

    Services Page
    Services page implementation

---
+++ services.php
@@ -0,0 +1,153 @@
+<?php /* Template Name: Services */ ?>
+<?php include 'header.php' ?>
+
+<main>
+<h1>Our Services</h1>
+<hr>
+
+<div class="row">
+  <div class="col m6">
+    <h2>LET VISBANKING CHANGE YOUR PERSPECTIVE ON BANKING DATA
+</h2>
+<p>
+  Gone are the days when financial professionals were forced to comb through Excel sheets of flat data to compare different banks' performance. With Visbanking, the future of banking data analysis is now. We provide the banking intelligence and do-it-for-me (DIFM) reports you need to visualize the latest data on every bank in the United States. With your subscription to our innovative services, you can rest assured that you will always have the timely and accurate information you need to ensure that you stay on top of the latest developments in the banking industry.
+<br>
+Our commitment to completeness of vision extends to every area of our services. That commitment is why we deliver the wide range of data you need to make the most effective decisions about your business, including vital information on performance, financials, market share, geography, personnel, and regulatory actions. That completeness of vision also extends to our ability to execute, as we strive each day to understand your business challenges so that we can deliver the innovative products and services you need to enhance your success.
+</p>
+  </div>
+
+  <div class="col m6">
+
+  </div>
+
+</div>
+
+<hr>
+<h2 class="text-center">THE BANKING INFORMATION SERVICES YOU NEED TO ENHANCE YOUR DECISION-MAKING
+</h2>
+
+<div class="row">
+  <div class="col m4">
+    <div class="service-card">
+      <div class="service-card-title">
+        Competitive Analysis
+      </div>
+      <div class="service-card-text">
+        Our service allows you to quickly and easily view any group of competitor banks to assess and compare their performance. That intelligence can help you perform complex analysis easier than ever before, enabling better decision-making that will improve your company's bottom line. Quickly access details like balance sheets, financial statements, current activities, and insights in the news. With a few clicks, those and other key data points can be quickly compiled to inform your analysis.
+      </div>
+    </div>
+
+  </div>
+
+  <div class="col m4">
+    <div class="service-card">
+      <div class="service-card-title">
+        Market Share
+      </div>
+      <div class="service-card-text">
+      When you need to make critical decisions that impact your company's market share, it is vital to understand everything that you can about your competitors. With Visbanking's services, you can quickly define market parameters and get the intelligence you need to measure the market share of all banks in that target market and adapt your strategies to improve your bank's performance.
+      </div>
+    </div>
+
+  </div>
+
+  <div class="col m4">
+    <div class="service-card">
+      <div class="service-card-title">
+      M&A due dilligence
+      </div>
+      <div class="service-card-text">
+      Before any successful merger or acquisition, it is essential to perform due diligence on important criteria like credit quality metrics. If you have a merger looming on the horizon, our tools can help you gain the insight you need to know before moving to combine portfolios. This data can help ensure that your merger or acquisition can be completed without unforeseen complications.
+      </div>
+    </div>
+
+  </div>
+
+  <div class="col m4">
+    <div class="service-card">
+      <div class="service-card-title">
+        Credit Quality Trends
+      </div>
+      <div class="service-card-text">
+      It can be time-consuming to assess credit quality trends in the banking industry, including key information about things like non-performing loans viewed by asset class. Visbanking's services enable you to view any custom group of competitor banks in a way that simplifies your analysis of their comparative credit quality performance. In addition, we simplify the process of comparing past due loan trends and relevant reserves, enabling you to easily compare trends in 20 and 90-day past due loans alongside the allowance for loan and lease loss trends, to quickly identify anomalies.
+      </div>
+    </div>
+
+  </div>
+
+  <div class="col m4">
+    <div class="service-card">
+      <div class="service-card-title">
+        Geographic Coverage
+      </div>
+      <div class="service-card-text">
+      One of the biggest challenges to new market entry is learning about existing competition in that area. If you are considering a move into a new market, Visbanking can provide the information you need to accurately assess your banking competitors in that geographical area, including key details about those banks' deposit sources and lending participation.
+      </div>
+    </div>
+
+  </div>
+
+  <div class="col m4">
+    <div class="service-card">
+      <div class="service-card-title">
+        Insight into Deposit and Loan Growth or Decline
+      </div>
+      <div class="service-card-text">
+      Does your business need to learn more about deposit or loan growth or decline throughout the U.S.? Is a comprehensive understanding of macro trends a vital part of your decision-making process? If so, our services can provide you with the ability to quickly access information about the entire U.S. banking landscape, including deposit and loan growth over time, net interest margins, and the cost of funds, deposits, and non-interest income.
+      </div>
+    </div>
+
+  </div>
+</div>
+<hr>
+
+<h2>OUR PLATFORM</h2>
+<p>
+  Visbanking's services do more than just provide access to raw data, charts, and graphs. Our platform delivers an interactive experience that allows you to see key data in an easily understood and visual way. Our DIFM approach saves you time and helps to ensure that your data visualizations are showing you exactly what you want to see, when you want to see it.
+  <br>
+  <br>
+  When you rely on Visbanking, you get the security and peace of mind that comes from knowing that all your key decisions are being made based on timely, accurate data - and not just opinion or conjecture. To learn more about how our banking intelligence services can benefit you, contact us today.
+</p>
+
+<div class="row">
+
+  <div class="col m6 product-img">
+    <img class="img-dashboards" src="<?php bloginfo('template_url'); ?>/img/dashboard1.png" alt="">
+  </div>
+
+  <div class="col m6 product-img">
+    <img class="img-dashboards " src="<?php bloginfo('template_url'); ?>/img/dashboard2.png" alt="">
+  </div>
+
+</div>
+
+<div class="row">
+
+  <div class="col m6 product-img">
+    <img class="img-dashboards" src="<?php bloginfo('template_url'); ?>/img/dashboard3.png" alt="">
+  </div>
+
+  <div class="col m6 product-img">
+    <img class="img-dashboards " src="<?php bloginfo('template_url'); ?>/img/dashboard4.png" alt="">
+  </div>
+
+</div>
+
+<div class="row">
+
+  <div class="col m6 product-img">
+    <img class="img-dashboards" src="<?php bloginfo('template_url'); ?>/img/dashboard5.png" alt="">
+  </div>
+
+  <div class="col m6 product-img">
+    <img class="img-dashboards " src="<?php bloginfo('template_url'); ?>/img/dashboard6.png" alt="">
+  </div>
+
+</div>
+
+
+
+
+
+</main>
+<?php include 'footer.php' ?>
