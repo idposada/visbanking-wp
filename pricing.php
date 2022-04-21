955cdd84062f9e4b62086d6dd24b1d539ee1728b
Ivan Dario Posada <ivandarioposada@yahoo.com>
2022-04-20 21:46:33 -0500

    Implemented Pricing Page

---
+++ pricing.php
@@ -0,0 +1,165 @@
+<?php /* Template Name: Pricing */ ?>
+<?php include 'header.php' ?>
+
+<main>
+  <h1 class="text-center">Banks Visualized</h1>
+  <hr>
+  <h2 class="text-center">Subscription plans to meet your needs</h2>
+
+  <div class="row">
+
+    <div class="col m3 plan-col">
+      <div class="plan-name">
+        Free
+      </div>
+      <div class="plan-price">
+
+        $0
+      </div>
+
+      <hr>
+
+      <p class="plan-details">
+
+        <span class="material-icons">check_circle
+        </span>Single bank reports
+
+        <br>
+        <span class="material-icons">check_circle
+        </span>Bank industry reports
+        <br>
+        <span class="material-icons">check_circle
+        </span>Top bank lists
+        <br>
+        <span class="material-icons">check_circle
+        </span>  Bank footprints
+        <br>
+        <span class="material-icons">check_circle
+        </span>5 banks per quarter
+        <br>
+      </p>
+
+
+      <div class="plan-button">
+        <button type="button" name="button">SUBSCRIBE</button>
+      </div>
+
+
+
+
+    </div>
+    <div class="col m3 plan-featured">
+      <div class="plan-name">
+        professional
+      </div>
+      <div class="plan-price">
+
+        $0
+      </div>
+
+      <hr>
+
+      <p class="plan-details">
+
+        <span class="material-icons">check_circle
+        </span>Single bank reports
+
+        <br>
+        <span class="material-icons">check_circle
+        </span>Bank industry reports
+        <br>
+        <span class="material-icons">check_circle
+        </span>Top bank lists
+        <br>
+        <span class="material-icons">check_circle
+        </span>  Bank footprints
+        <br>
+        <span class="material-icons">check_circle
+        </span>5 banks per quarter
+        <br>
+      </p>
+
+
+      <div class="plan-button-featured">
+        <button type="button" name="button">SUBSCRIBE</button>
+      </div>
+    </div>
+
+    <div class="col m3 plan-col">
+      <div class="plan-name">
+        Free
+      </div>
+      <div class="plan-price">
+
+        $0
+      </div>
+
+      <hr>
+
+      <p class="plan-details">
+
+        <span class="material-icons">check_circle
+        </span>Single bank reports
+
+        <br>
+        <span class="material-icons">check_circle
+        </span>Bank industry reports
+        <br>
+        <span class="material-icons">check_circle
+        </span>Top bank lists
+        <br>
+        <span class="material-icons">check_circle
+        </span>  Bank footprints
+        <br>
+        <span class="material-icons">check_circle
+        </span>5 banks per quarter
+        <br>
+      </p>
+
+
+      <div class="plan-button">
+        <button type="button" name="button">SUBSCRIBE</button>
+      </div>
+    </div>
+
+    <div class="col m3 plan-col">
+      <div class="plan-name">
+        Free
+      </div>
+      <div class="plan-price">
+
+        $0
+      </div>
+
+      <hr>
+
+      <p class="plan-details">
+
+        <span class="material-icons">check_circle
+        </span>Single bank reports
+
+        <br>
+        <span class="material-icons">check_circle
+        </span>Bank industry reports
+        <br>
+        <span class="material-icons">check_circle
+        </span>Top bank lists
+        <br>
+        <span class="material-icons">check_circle
+        </span>  Bank footprints
+        <br>
+        <span class="material-icons">check_circle
+        </span>5 banks per quarter
+        <br>
+      </p>
+
+
+      <div class="plan-button">
+        <button type="button" name="button">SUBSCRIBE</button>
+      </div>
+    </div>
+
+  </div>
+</main>
+
+<?php include 'footer.php' ?>
