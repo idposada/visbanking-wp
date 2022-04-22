6619d4c935622c8de709cc4aa3a8f78f82f4416a
Ivan Dario Posada <ivandarioposada@yahoo.com>
2022-04-22 16:58:33 -0500

    Implementation of Insights page

---
+++ insights.php
@@ -0,0 +1,311 @@
+<?php /* Template Name: Insights */ ?>
+<?php include 'header.php' ?>
+<main>
+<h1 class="text-center">Insights</h1>
+  <hr>
+
+  <div class="row featured-post-insights">
+    <div class="col m6 featured-post-image">
+      <img src="<?php bloginfo('template_url'); ?>/img/headerImage.jpg" alt="">
+    </div>
+
+    <div class="col m6 featured-pots-info">
+      <div class="featured-pots-title">
+        Bitcoin Price Sluggish as Fed Signals Multiple Rate Hikes for 2022
+      </div>
+      <hr class="line-footer">
+      <div class="featured-post-resume">
+        The price of Bitcoin remains well below its record November 2021 high of nearly $69,000, as regulatory and interest rate concerns continues ...
+      </div>
+      <div class="post-date">
+        4/11/2022
+      </div>
+      <button type="button" name="button" class="readmore-button">READ MORE >></button>
+
+    </div>
+
+  </div>
+
+  <hr>
+
+  <h2>Featured news</h2>
+
+  <div class="row">
+  <div class="col m4">
+    <div class="card vis-card">
+      <div class="card-image">
+        <img src="<?php bloginfo('template_url'); ?>/img/news1.png" alt="">
+      </div>
+      <div class="card-title">
+        Asset Information
+      </div>
+
+      <div class="card-action home-learn-more">
+        <a href="#">Learn More</a>
+
+      </div>
+
+
+    </div>
+  </div>
+
+  <div class="col m4">
+    <div class="card vis-card">
+      <div class="card-image">
+        <img src="<?php bloginfo('template_url'); ?>/img/news1.png" alt="">
+      </div>
+      <div class="card-title">
+        Asset Information
+      </div>
+
+      <div class="card-action home-learn-more">
+        <a href="#">Learn More</a>
+
+      </div>
+
+
+    </div>
+  </div>
+
+  <div class="col m4">
+    <div class="card vis-card">
+      <div class="card-image">
+        <img src="<?php bloginfo('template_url'); ?>/img/news1.png" alt="">
+      </div>
+      <div class="card-title">
+        Asset Information
+      </div>
+
+      <div class="card-action home-learn-more">
+        <a href="#">Learn More</a>
+
+      </div>
+
+
+    </div>
+  </div>
+
+  </div>
+
+  <section>
+    <div class="row">
+      <h2>NEWS</h2>
+    </div>
+    <div class="row categories">
+      <div class="col m8">
+        <div class="row category">
+          <h3>Business</h3>
+          <hr>
+          <div class="col m6 ">
+
+            <div class="card vis-card">
+              <div class="card-image">
+                <img src="<?php bloginfo('template_url'); ?>/img/news1.png" alt="">
+              </div>
+              <div class="card-title">
+                Asset Information
+              </div>
+
+              <div class="card-action home-learn-more">
+                <a href="#">Learn More</a>
+
+              </div>
+
+
+            </div>
+
+          </div>
+
+          <div class="col m6">
+            <div class="card vis-card">
+              <div class="card-image">
+                <img src="<?php bloginfo('template_url'); ?>/img/news1.png" alt="">
+              </div>
+              <div class="card-title">
+                Asset Information
+              </div>
+
+              <div class="card-action home-learn-more">
+                <a href="#">Learn More</a>
+
+              </div>
+
+
+            </div>
+          </div>
+        </div>
+
+        <div class="row category">
+          <h3>Banks</h3>
+          <hr>
+          <div class="col m6">
+            <div class="card vis-card">
+              <div class="card-image">
+                <img src="<?php bloginfo('template_url'); ?>/img/news1.png" alt="">
+              </div>
+              <div class="card-title">
+                Asset Information
+              </div>
+
+              <div class="card-action home-learn-more">
+                <a href="#">Learn More</a>
+
+              </div>
+
+
+            </div>
+          </div>
+
+          <div class="col m6">
+            <div class="card vis-card">
+              <div class="card-image">
+                <img src="<?php bloginfo('template_url'); ?>/img/news1.png" alt="">
+              </div>
+              <div class="card-title">
+                Asset Information
+              </div>
+
+              <div class="card-action home-learn-more">
+                <a href="#">Learn More</a>
+
+              </div>
+
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col m4 last-mews">
+        <h3>Last news</h3>
+        <hr>
+        <div class="featured-post">
+          <div class="featured-post-img col m6">
+            <img src="<?php bloginfo('template_url'); ?>/img/news1.png" alt="">
+          </div>
+          <div class="post-info col m6">
+
+
+          <div class="featured-post-tit">
+            Asset Information
+          </div>
+          <div class="home-learn-more">
+            <p>Read More... </p>
+          </div>
+          </div>
+        </div>
+
+        <div class="featured-post">
+          <div class="featured-post-img col m6">
+            <img src="<?php bloginfo('template_url'); ?>/img/news1.png" alt="">
+          </div>
+          <div class="post-info col m6">
+
+
+          <div class="featured-post-tit">
+            Asset Information
+          </div>
+          <div class="home-learn-more">
+            <p>Read More... </p>
+          </div>
+          </div>
+        </div>
+
+        <div class="featured-post">
+          <div class="featured-post-img col m6">
+            <img src="<?php bloginfo('template_url'); ?>/img/news1.png" alt="">
+          </div>
+          <div class="post-info col m6">
+
+
+          <div class="featured-post-tit">
+            Asset Information
+          </div>
+          <div class="home-learn-more">
+            <p>Read More... </p>
+          </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <hr>
+  </section>
+
+  <section class="all-categories">
+    <div class="row">
+     <div class="col s12">
+       <ul class="tabs">
+         <li class="tab tab-vis"><a class="active" href="#test1">All</a></li>
+         <li class="tab tab-vis"><a  href="#test2">Business</a></li>
+         <li class="tab tab-vis"><a href="#test3">Banks</a></li>
+         <li class="tab tab-vis"><a href="#test4">Financial</a></li>
+         <li class="tab tab-vis"><a href="#test4">Market</a></li>
+         <li class="tab tab-vis"><a href="#test4">Politics</a></li>
+       </ul>
+
+     </div>
+         <hr>
+     <div id="test1" class="row">
+       <div class="col m4">
+         <div class="featured-post">
+           <div class="featured-post-img col m6">
+             <img src="<?php bloginfo('template_url'); ?>/img/news1.png" alt="">
+           </div>
+           <div class="post-info col m6">
+
+
+           <div class="featured-post-tit">
+             Asset Information
+           </div>
+           <div class="home-learn-more">
+             <p>Read More... </p>
+           </div>
+           </div>
+         </div>
+       </div>
+       <div class="col m4">
+         <div class="featured-post">
+           <div class="featured-post-img col m6">
+             <img src="<?php bloginfo('template_url'); ?>/img/news1.png" alt="">
+           </div>
+           <div class="post-info col m6">
+
+
+           <div class="featured-post-tit">
+             Asset Information
+           </div>
+           <div class="home-learn-more">
+             <p>Read More... </p>
+           </div>
+           </div>
+         </div>
+       </div>
+       <div class="col m4">
+         <div class="featured-post">
+           <div class="featured-post-img col m6">
+             <img src="<?php bloginfo('template_url'); ?>/img/news1.png" alt="">
+           </div>
+           <div class="post-info col m6">
+
+
+           <div class="featured-post-tit">
+             Asset Information
+           </div>
+           <div class="home-learn-more">
+             <p>Read More... </p>
+           </div>
+           </div>
+         </div>
+       </div>
+     </div>
+     <div id="test2" class="col s12">Test 2</div>
+     <div id="test3" class="col s12">Test 3</div>
+     <div id="test4" class="col s12">Test 4</div>
+   </div>
+
+
+  </section>
+</main>
+
+<?php include 'footer.php' ?>
